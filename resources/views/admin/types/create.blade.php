5906ee388aaf804332793020e2e7b0376727c8f2
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-12 15:42:12 +0100

    make table type in db

---
+++ resources/views/admin/types/create.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.admin')
+@section('content')
+    <h1>Create Project</h1>
+    <div class="row bg-white">
+        <div class="col-12">
+            <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data" class="p-4">
+                @csrf
+                <div class="mb-3">
+                    {{-- name_proj --}}
+                    <label for="title" class="form-label">Titolo</label>
+                    <input type="text" class="form-control @error('name_proj') is-invalid @enderror" id="name_proj"
+                        name="name_proj">
+                    @error('name_proj')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                {{-- img_cover --}}
+                <div class="mb-3">
+                    <label for="cover_image" class="form-label">Immagine</label>
+                    <input type="file" name="cover_image" id="cover_image"
+                        class="form-control  @error('cover_image') is-invalid @enderror">
+                    @error('cover_image')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                {{-- description --}}
+                <div class="mb-3">
+                    <label for="description" class="form-label">description</label>
+                    <textarea class="form-control" id="description" name="description"></textarea>
+                </div>
+                {{-- dev lang  --}}
+                <div class="mb-3">
+                    <label for="dev_lang" class="form-label">Linguaggi di programmazione</label>
+                    <input type="text" class="form-control @error('dev_lang') is-invalid @enderror" id="dev_lang"
+                        name="dev_lang">
+                    @error('dev_lang')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                {{-- dev_framework --}}
+                <div class="mb-3">
+                    <label for="dev_framework" class="form-label">framework usati </label>
+                    <input class="form-control" id="dev_framework" name="dev_framework">
+                </div>
+                {{-- team --}}
+                <div class="mb-3">
+                    <label for="team" class="form-label">team </label>
+                    <input class="form-control" id="team" name="team">
+                </div>
+
+                {{-- lvl_diff --}}
+                <label for="lvl_dif" class="form-label">livello di dificolta </label>
+                <input type="number" class="form-control @error('lvl_dif') is-invalid @enderror" id="lvl_dif"
+                    name="lvl_dif">
+                @error('lvl_dif')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+        </div>
+        {{-- linkgit --}}
+        <div class="mb-3">
+            <label for="link_git" class="form-label">GhitHub </label>
+            <input class="form-control" id="link_git" name="link_git">
+        </div>
+
+        <button type="submit" class="btn btn-success">Submit</button>
+        <button type="reset" class="btn btn-primary">Reset</button>
+        </form>
+    </div>
+    </div>
+@endsection
