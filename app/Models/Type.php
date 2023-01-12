509007727d2e77aa51087e1cb0cff2584859fe2f
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-12 16:00:58 +0100

    seed db

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'workflow',
+        'slug',
+
+    ];
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
 }
